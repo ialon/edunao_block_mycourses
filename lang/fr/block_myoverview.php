08ec00d3f3cc667e07644ac50ca21ac4949a12ee
Josemaria Bolanos <josemabol@gmail.com>
2024-08-16 10:18:06 -0600

    Added french language

---
+++ lang/fr/block_myoverview.php
@@ -0,0 +1,108 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for the My overview block.
+ *
+ * @package    block_myoverview
+ * @copyright  Mark Nelson <markn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['allincludinghidden'] = 'Tous (y compris ceux supprimés de la vue)';
+$string['all'] = 'Tous';
+$string['addtofavourites'] = 'Ajouter aux favoris';
+$string['aria:addtofavourites'] = 'Ajouter aux favoris pour';
+$string['aria:allcoursesincludinghidden'] = 'Afficher tous les cours';
+$string['aria:allcourses'] = 'Afficher tous les cours sauf ceux supprimés de la vue';
+$string['aria:card'] = 'Passer à la vue en cartes';
+$string['aria:controls'] = 'Contrôles de l\'aperçu du cours';
+$string['aria:courseactions'] = 'Actions pour le cours';
+$string['aria:coursesummary'] = 'Texte de résumé du cours :';
+$string['aria:courseprogress'] = 'Progression du cours :';
+$string['aria:customfield'] = 'Afficher les cours {$a}';
+$string['aria:displaydropdown'] = 'Afficher le menu déroulant';
+$string['aria:favourites'] = 'Afficher uniquement les cours favoris';
+$string['aria:future'] = 'Afficher les cours à venir';
+$string['aria:groupingdropdown'] = 'Menu déroulant de regroupement';
+$string['aria:inprogress'] = 'Afficher les cours en cours';
+$string['aria:list'] = 'Passer à la vue en liste';
+$string['aria:past'] = 'Afficher les cours passés';
+$string['aria:removefromfavourites'] = 'Supprimer des favoris pour';
+$string['aria:summary'] = 'Passer à la vue en résumé';
+$string['aria:sortingdropdown'] = 'Menu déroulant de tri';
+$string['availablegroupings'] = 'Filtres disponibles';
+$string['availablegroupings_desc'] = 'Filtres de cours disponibles pour la sélection par les utilisateurs. Si aucun n\'est sélectionné, tous les cours seront affichés.';
+$string['card'] = 'Carte';
+$string['cards'] = 'Cartes';
+$string['courseprogress'] = 'Progression du cours :';
+$string['completepercent'] = '{$a}% terminé';
+$string['createcourse'] = 'Créer un cours';
+$string['customfield'] = 'Champ personnalisé';
+$string['customfiltergrouping'] = 'Champ à utiliser';
+$string['customfiltergrouping_nofields'] = 'Cette option nécessite la configuration d\'un champ personnalisé de cours visible par tous.';
+$string['displaycategories'] = 'Afficher les catégories';
+$string['displaycategories_help'] = 'Afficher la catégorie du cours sur les éléments du tableau de bord, y compris les cartes, les éléments de liste et les éléments de résumé.';
+$string['favourites'] = 'Favoris';
+$string['future'] = 'À venir';
+$string['inprogress'] = 'En cours';
+$string['lastaccessed'] = 'Dernier accès';
+$string['layouts'] = 'Mises en page disponibles';
+$string['layouts_help'] = 'Mises en page de l\'aperçu du cours disponibles pour la sélection par les utilisateurs. Si aucune n\'est sélectionnée, la mise en page en cartes sera utilisée.';
+$string['list'] = 'Liste';
+$string['myoverview:myaddinstance'] = 'Ajouter un nouveau bloc d\'aperçu du cours au tableau de bord';
+$string['nocustomvalue'] = 'Aucun {$a}';
+$string['past'] = 'Passé';
+$string['pluginname'] = 'Aperçu du cours';
+$string['privacy:metadata:overviewsortpreference'] = 'La préférence de tri du bloc d\'aperçu du cours.';
+$string['privacy:metadata:overviewviewpreference'] = 'La préférence d\'affichage du bloc d\'aperçu du cours.';
+$string['privacy:metadata:overviewgroupingpreference'] = 'La préférence de regroupement du bloc d\'aperçu du cours.';
+$string['privacy:metadata:overviewpagingpreference'] = 'La préférence de pagination du bloc d\'aperçu du cours.';
+$string['removefromfavourites'] = 'Retirer des favoris';
+$string['searchcourses'] = "Rechercher des cours";
+$string['shortname'] = 'Nom court';
+$string['summary'] = 'Résumé';
+$string['title'] = 'Nom du cours';
+$string['aria:hidecourse'] = 'Supprimer {$a} de la vue';
+$string['aria:showcourse'] = 'Restaurer {$a} dans la vue';
+$string['aria:invisiblecourse'] = 'Rendre le cours {$a} invisible pour les apprenants';
+$string['aria:visiblecourse'] = 'Rendre le cours {$a} visible pour les apprenants';
+$string['aria:hiddencourses'] = 'Afficher les cours supprimés de la vue';
+$string['hidden'] = 'Cours supprimés de la vue';
+$string['hidecourse'] = 'Supprimer de la vue';
+$string['hiddencourses'] = 'Supprimés de la vue';
+$string['show'] = 'Restaurer la vue';
+$string['invisiblecourse'] = 'Masquer le cours';
+$string['visiblecourse'] = 'Afficher le cours';
+$string['sortbytitle'] = 'Trier par nom du cours';
+$string['sortbylastaccessed'] = 'Trier par dernier accès';
+$string['sortbyshortname'] = 'Trier par nom court';
+$string['privacy:request:preference:set'] = 'La valeur du paramètre "{$a->name}" était "{$a->value}"';
+$string['viewquickstart'] = 'Voir le guide de démarrage rapide';
+$string['zero_default_title'] = 'Vous n\'êtes inscrit à aucun cours';
+$string['zero_default_intro'] = 'Une fois inscrit à un cours, il apparaîtra ici.';
+$string['zero_request_title'] = 'Demandez votre premier cours';
+$string['zero_request_intro'] = 'Besoin d\'aide pour commencer ? Consultez la <a href="{$a->dochref}" title="{$a->doctitle}" target="{$a->doctarget}">documentation Moodle</a> ou faites vos premiers pas avec notre <a href="{$a->quickhref}" title="{$a->quicktitle}" target="{$a->quicktarget}">guide de démarrage rapide</a>.';
+$string['zero_nocourses_title'] = 'Créez votre premier cours';
+$string['zero_nocourses_intro'] = 'Besoin d\'aide pour commencer ? Consultez la <a href="{$a->dochref}" title="{$a->doctitle}" target="{$a->doctarget}">documentation Moodle</a> ou faites vos premiers pas avec notre guide de démarrage rapide.';
+$string['clearsearch'] = "Effacer la recherche";
+$string['aria:lastaccessed'] = 'Trier les cours par date de dernier accès';
+$string['aria:shortname'] = 'Trier les cours par nom court';
+$string['aria:title'] = 'Trier les cours par nom du cours';
+
+$string['displayrole'] = 'Mes cours en tant que';
+$string['teacher'] = 'Enseignant';
+$string['student'] = 'Étudiant';
