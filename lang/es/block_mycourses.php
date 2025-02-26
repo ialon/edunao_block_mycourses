13437607df169c3f112ad61c2d908ea64aa950f2
Josemaria Bolanos <josemabol@gmail.com>
2025-02-25 18:36:41 -0600

    Added italian and spanish translations

---
+++ lang/es/block_mycourses.php
@@ -0,0 +1,122 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for the My Courses block.
+ *
+ * @package    block_mycourses
+ * @copyright  Mark Nelson <markn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+ $string['allincludinghidden'] = 'Todos (incluidos los cursos escondidos)';
+$string['all'] = 'Todos';
+$string['addtofavourites'] = 'Marcar como favorito';
+$string['aria:addtofavourites'] = 'Favorito para';
+$string['aria:allcoursesincludinghidden'] = 'Mostrar todos los cursos';
+$string['aria:allcourses'] = 'Mostrar todos los cursos excepto los escondidos';
+$string['aria:card'] = 'Cambiar a vista de tarjetas';
+$string['aria:controls'] = 'Controles de Mis Cursos';
+$string['aria:courseactions'] = 'Acciones para el curso';
+$string['aria:coursesummary'] = 'Resumen del curso:';
+$string['aria:courseprogress'] = 'Progreso del curso:';
+$string['aria:customfield'] = 'Mostrar cursos de {$a}';
+$string['aria:displaydropdown'] = 'Mostrar menú desplegable';
+$string['aria:favourites'] = 'Mostrar solo cursos favoritos';
+$string['aria:future'] = 'Mostrar cursos futuros';
+$string['aria:groupingdropdown'] = 'Menú desplegable de agrupación';
+$string['aria:inprogress'] = 'Mostrar cursos en progreso';
+$string['aria:list'] = 'Cambiar a vista de lista';
+$string['aria:past'] = 'Mostrar cursos pasados';
+$string['aria:removefromfavourites'] = 'Quitar de favoritos';
+$string['aria:summary'] = 'Cambiar a vista de resumen';
+$string['aria:sortingdropdown'] = 'Menú desplegable de orden';
+$string['availablegroupings'] = 'Filtros disponibles';
+$string['availablegroupings_desc'] = 'Filtros disponibles para los usuarios. Si no se selecciona ninguno, se mostrarán todos los cursos.';
+$string['card'] = 'Tarjeta';
+$string['cards'] = 'Tarjetas';
+$string['courseprogress'] = 'Progreso del curso:';
+$string['completepercent'] = '{$a}% completado';
+$string['createcourse'] = 'Crear curso';
+$string['customfield'] = 'Campo personalizado';
+$string['customfiltergrouping'] = 'Campo a utilizar';
+$string['customfiltergrouping_nofields'] = 'Esta opción requiere que se configure un campo personalizado del curso y sea visible para todos.';
+$string['displaycategories'] = 'Mostrar categorías';
+$string['displaycategories_help'] = 'Mostrar la categoría del curso en el tablero, incluidas las vistas de tarjeta, lista y resumen.';
+$string['favourites'] = 'Favoritos';
+$string['future'] = 'Futuros';
+$string['inprogress'] = 'En progreso';
+$string['lastaccessed'] = 'Último acceso';
+$string['layouts'] = 'Vistas disponibles';
+$string['layouts_help'] = 'Vistas disponibles para selección por los usuarios. Si no se selecciona ninguno, se usará la vista de tarjeta.';
+$string['list'] = 'Lista';
+$string['mycourses:myaddinstance'] = 'Agregar un nuevo bloque de Mis Cursos al Tablero';
+$string['nocustomvalue'] = 'No {$a}';
+$string['past'] = 'Pasados';
+$string['pluginname'] = 'Mis Cursos';
+$string['blocktitle:teacher'] = 'Cursos impartidos';
+$string['blocktitle:student'] = 'Mi aprendizaje';
+$string['privacy:metadata:mycoursessortpreference'] = 'Preferencia de orden del bloque Mis Cursos.';
+$string['privacy:metadata:mycoursesviewpreference'] = 'Preferencia de vista del bloque Mis Cursos.';
+$string['privacy:metadata:mycoursesgroupingpreference'] = 'Preferencia de agrupación del bloque Mis Cursos.';
+$string['privacy:metadata:mycoursespagingpreference'] = 'Preferencia de paginación del bloque Mis Cursos.';
+$string['removefromfavourites'] = 'Quitar de favoritos';
+$string['searchcourses'] = "Buscar cursos";
+$string['shortname'] = 'Nombre corto';
+$string['summary'] = 'Resumen';
+$string['title'] = 'Nombre del curso';
+$string['aria:hidecourse'] = 'Esconder {$a} de la vista';
+$string['aria:showcourse'] = 'Mostrar {$a} a la vista';
+$string['aria:invisiblecourse'] = 'Hacer {$a} invisible para los estudiantes';
+$string['aria:visiblecourse'] = 'Hacer {$a} visible para los estudiantes';
+$string['aria:hiddencourses'] = 'Mostrar cursos escondidos de la vista';
+$string['hidden'] = 'Cursos escondidos de la vista';
+$string['hidecourse'] = 'Esconder de la vista';
+$string['hiddencourses'] = 'Escondidos de la vista';
+$string['show'] = 'Mostrar en la vista';
+$string['invisiblecourse'] = 'Curso oculto';
+$string['visiblecourse'] = 'Curso visible';
+$string['sortbytitle'] = 'Ordenar por nombre del curso';
+$string['sortbylastaccessed'] = 'Ordenar por último acceso';
+$string['sortbyshortname'] = 'Ordenar por nombre corto';
+$string['privacy:request:preference:set'] = 'El valor de la configuración \'{$a->name}\' era \'{$a->value}\'';
+$string['viewquickstart'] = 'Ver guía de inicio rápido';
+$string['zero_default_title'] = 'No estás inscrito en ningún curso';
+$string['zero_default_intro'] = 'Una vez que estés inscrito en un curso, aparecerá aquí.';
+$string['zero_request_title'] = 'Solicita tu primer curso';
+$string['zero_request_intro'] = '¿Necesitas ayuda para comenzar? Consulta la <a href="{$a->dochref}" title="{$a->doctitle}" target="{$a->doctarget}">documentación de Moodle</a> o da tus primeros pasos con nuestra <a href="{$a->quickhref}" title="{$a->quicktitle}" target="{$a->quicktarget}">guía de inicio rápido</a>.';
+$string['zero_nocourses_title'] = 'Crea tu primer curso';
+$string['zero_nocourses_intro'] = '¿Necesitas ayuda para comenzar? Consulta la <a href="{$a->dochref}" title="{$a->doctitle}" target="{$a->doctarget}">documentación de Moodle</a> o da tus primeros pasos con nuestra guía de inicio rápido.';
+
+// Obsoleto desde Moodle 4.0.
+$string['clearsearch'] = "Limpiar búsqueda";
+$string['aria:lastaccessed'] = 'Ordenar cursos por fecha de último acceso';
+$string['aria:shortname'] = 'Ordenar cursos por nombre corto del curso';
+$string['aria:title'] = 'Ordenar cursos por nombre del curso';
+
+$string['displayrole'] = 'Mis cursos como';
+$string['teacher'] = 'Profesor';
+$string['student'] = 'Estudiante';
+
+// Cadenas KPI
+$string['kpi:learners'] = 'Estudiantes:';
+$string['kpi:learners:enrolled'] = '{$a} inscritos';
+$string['kpi:learners:started'] = '{$a} han iniciado';
+$string['kpi:learners:completed'] = '{$a} han completado';
+$string['kpi:learners:shared'] = '{$a} han compartido';
+$string['kpi:timetocomplete'] = 'Duración promedio del curso:';
+$string['kpi:completiongrade'] = 'Calificación promedio:';
+$string['kpi:lastaccessed'] = 'Último acceso:';
---
+++ lang/it/block_mycourses.php
@@ -0,0 +1,121 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for the My Courses block.
+ *
+ * @package    block_mycourses
+ * @copyright  Mark Nelson <markn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['allincludinghidden'] = 'Tutti (inclusi rimossi dalla vista)';
+$string['all'] = 'Tutti';
+$string['addtofavourites'] = 'Aggiungi ai preferiti';
+$string['aria:addtofavourites'] = 'Aggiungi ai preferiti per';
+$string['aria:allcoursesincludinghidden'] = 'Mostra tutti i corsi';
+$string['aria:allcourses'] = 'Mostra tutti i corsi tranne quelli rimossi dalla vista';
+$string['aria:card'] = 'Passa alla vista a schede';
+$string['aria:controls'] = 'Controlli dei miei corsi';
+$string['aria:courseactions'] = 'Azioni per il corso';
+$string['aria:coursesummary'] = 'Testo del sommario del corso:';
+$string['aria:courseprogress'] = 'Progresso del corso:';
+$string['aria:customfield'] = 'Mostra corsi {$a}';
+$string['aria:displaydropdown'] = 'Mostra menu a discesa';
+$string['aria:favourites'] = 'Mostra solo i corsi preferiti';
+$string['aria:future'] = 'Mostra corsi futuri';
+$string['aria:groupingdropdown'] = 'Menu a discesa di raggruppamento';
+$string['aria:inprogress'] = 'Mostra corsi in corso';
+$string['aria:list'] = 'Passa alla vista a lista';
+$string['aria:past'] = 'Mostra corsi passati';
+$string['aria:removefromfavourites'] = 'Rimuovi dai preferiti per';
+$string['aria:summary'] = 'Passa alla vista sommario';
+$string['aria:sortingdropdown'] = 'Menu a discesa di ordinamento';
+$string['availablegroupings'] = 'Filtri disponibili';
+$string['availablegroupings_desc'] = 'Filtri dei corsi disponibili per la selezione da parte degli utenti. Se nessuno è selezionato, verranno mostrati tutti i corsi.';
+$string['card'] = 'Scheda';
+$string['cards'] = 'Schede';
+$string['courseprogress'] = 'Progresso del corso:';
+$string['completepercent'] = '{$a}% completato';
+$string['createcourse'] = 'Crea corso';
+$string['customfield'] = 'Campo personalizzato';
+$string['customfiltergrouping'] = 'Campo da utilizzare';
+$string['customfiltergrouping_nofields'] = 'Questa opzione richiede che sia impostato un campo personalizzato del corso e visibile a tutti.';
+$string['displaycategories'] = 'Mostra categorie';
+$string['displaycategories_help'] = 'Mostra la categoria del corso negli elementi del corso della dashboard, inclusi schede, elementi della lista e elementi del sommario.';
+$string['favourites'] = 'Preferiti';
+$string['future'] = 'Futuro';
+$string['inprogress'] = 'In corso';
+$string['lastaccessed'] = 'Ultimo accesso';
+$string['layouts'] = 'Layout disponibili';
+$string['layouts_help'] = 'Layout dei miei corsi disponibili per la selezione da parte degli utenti. Se nessuno è selezionato, verrà utilizzato il layout a schede.';
+$string['list'] = 'Lista';
+$string['mycourses:myaddinstance'] = 'Aggiungi un nuovo blocco I miei corsi alla Dashboard';
+$string['nocustomvalue'] = 'Nessun {$a}';
+$string['past'] = 'Passato';
+$string['pluginname'] = 'I miei corsi';
+$string['blocktitle:teacher'] = 'Corsi insegnati';
+$string['blocktitle:student'] = 'Il mio apprendimento';
+$string['privacy:metadata:mycoursessortpreference'] = 'La preferenza di ordinamento del blocco I miei corsi.';
+$string['privacy:metadata:mycoursesviewpreference'] = 'La preferenza di visualizzazione del blocco I miei corsi.';
+$string['privacy:metadata:mycoursesgroupingpreference'] = 'La preferenza di raggruppamento del blocco I miei corsi.';
+$string['privacy:metadata:mycoursespagingpreference'] = 'La preferenza di paginazione del blocco I miei corsi.';
+$string['removefromfavourites'] = 'Rimuovi dai preferiti';
+$string['searchcourses'] = "Cerca corsi";
+$string['shortname'] = 'Nome breve';
+$string['summary'] = 'Sommario';
+$string['title'] = 'Nome del corso';
+$string['aria:hidecourse'] = 'Rimuovi {$a} dalla vista';
+$string['aria:showcourse'] = 'Ripristina {$a} alla vista';
+$string['aria:invisiblecourse'] = 'Rendi il corso {$a} invisibile agli studenti';
+$string['aria:visiblecourse'] = 'Rendi il corso {$a} visibile agli studenti';
+$string['aria:hiddencourses'] = 'Mostra corsi rimossi dalla vista';
+$string['hidden'] = 'Corsi rimossi dalla vista';
+$string['hidecourse'] = 'Rimuovi dalla vista';
+$string['hiddencourses'] = 'Rimossi dalla vista';
+$string['show'] = 'Ripristina alla vista';
+$string['invisiblecourse'] = 'Nascondi corso';
+$string['visiblecourse'] = 'Mostra corso';
+$string['sortbytitle'] = 'Ordina per nome del corso';
+$string['sortbylastaccessed'] = 'Ordina per ultimo accesso';
+$string['sortbyshortname'] = 'Ordina per nome breve';
+$string['privacy:request:preference:set'] = 'Il valore dell\'impostazione \'{$a->name}\' era \'{$a->value}\'';
+$string['viewquickstart'] = 'Visualizza guida rapida';
+$string['zero_default_title'] = 'Non sei iscritto a nessun corso';
+$string['zero_default_intro'] = 'Una volta iscritto a un corso, apparirà qui.';
+$string['zero_request_title'] = 'Richiedi il tuo primo corso';
+$string['zero_request_intro'] = 'Hai bisogno di aiuto per iniziare? Consulta la <a href="{$a->dochref}" title="{$a->doctitle}" target="{$a->doctarget}">documentazione di Moodle</a> o fai i primi passi con la nostra <a href="{$a->quickhref}" title="{$a->quicktitle}" target="{$a->quicktarget}">guida rapida</a>.';
+$string['zero_nocourses_title'] = 'Crea il tuo primo corso';
+$string['zero_nocourses_intro'] = 'Hai bisogno di aiuto per iniziare? Consulta la <a href="{$a->dochref}" title="{$a->doctitle}" target="{$a->doctarget}">documentazione di Moodle</a> o fai i primi passi con la nostra guida rapida.';
+
+// Deprecato da Moodle 4.0.
+$string['clearsearch'] = "Cancella ricerca";
+$string['aria:lastaccessed'] = 'Ordina i corsi per data di ultimo accesso';
+$string['aria:shortname'] = 'Ordina i corsi per nome breve del corso';
+$string['aria:title'] = 'Ordina i corsi per nome del corso';
+
+$string['displayrole'] = 'I miei corsi come';
+$string['teacher'] = 'Insegnante';
+$string['student'] = 'Studente';
+
+// Stringhe KPI
+$string['kpi:learners'] = 'Studenti:';
+$string['kpi:learners:enrolled'] = '{$a} iscritti';
+$string['kpi:learners:started'] = '{$a} iniziati';
+$string['kpi:learners:completed'] = '{$a} completati';
+$string['kpi:learners:shared'] = '{$a} condivisi';
+$string['kpi:timetocomplete'] = 'Tempo medio per completare:';
+$string['kpi:completiongrade'] = 'Voto medio di completamento:';
+$string['kpi:lastaccessed'] = 'Ultimo accesso:';
